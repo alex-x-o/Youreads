61274c4621147e83e7d202a438eba633ea8857f0
alex-x-o <mi16048@alas.matf.bg.ac.rs>
2022-11-05 01:12:01 +0100

    started working on finding timestamps with books through comments

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 def get_channel_stats(youtube, channel_ids):
     
@@ -76,6 +77,15 @@ def get_video_details(youtube, video_ids):
     
     return pd.DataFrame(all_video_info)
 
+def get_book_related_videos(video_df):
+    titles = []
+    for i in range(len(video_df)):
+        title = video_df.loc[i, 'title']
+        if re.search(".*books?.*", title):
+            titles.append(title)
+            
+    return titles
+
 def get_comments_in_videos(youtube, video_ids):
     
     all_comments = []
@@ -83,17 +93,60 @@ def get_comments_in_videos(youtube, video_ids):
     for video_id in video_ids:
         try:
             request = youtube.commentThreads().list(
-                part="snippet,replies",
-                videoId=video_id
+                part="snippet",
+                videoId=video_id,
+                textFormat="plainText",
+                maxResults=100,
+                order="relevance",
             )
             response = request.execute()
 
-            comments_in_video = [comment['snippet']['topLevelComment']['snippet']['textOriginal'] for comment in response['items'][0:10]]
+            comments_in_video = [comment['snippet']['topLevelComment']['snippet']['textOriginal'] for comment in response['items'][0:100]]
             comments_in_video_info = {'video_id': video_id, 'comments': comments_in_video}
 
             all_comments.append(comments_in_video_info)
         except:
             print('Could not get comment for video ' + video_id)
+    
+    return pd.DataFrame(all_comments)
+
+def get_timestamps(comments_in_video):
+    new_comments = []
+    for i in range(len(comments_in_video)):
+        comment = comments_in_video[i]
+        if re.search(".*(timestamps?|[0-9]+\.|[0-9]{1,2}:[0-9]{2}).*", comment, re.I):
+            new_comments.append(comment)
+                
+    return new_comments
+
+def get_timestamp_comments(youtube, video_ids):
+    
+    all_comments = []
+    next_page_token = None
+    
+    for video_id in video_ids:
+        while True:
+            request = youtube.commentThreads().list(
+                part="snippet",
+                videoId=video_id,
+                textFormat="plainText",
+                maxResults=100,
+                order="relevance",
+                pageToken=next_page_token
+            )
+            response = request.execute()
+
+            comments_in_video = [comment['snippet']['topLevelComment']['snippet']['textOriginal'] for comment in response['items'][0:100]]
+            new_comments = get_timestamps(comments_in_video)
+            if not new_comments:
+                continue
+            comments_in_video_info = {'video_id': video_id, 'comments': new_comments}
+
+            all_comments.append(comments_in_video_info)
+            
+            next_page_token = response.get('nextPageToken')
+            if next_page_token is None:
+                break
     
     return pd.DataFrame(all_comments)
 
